dcb45dfc0c5200a4dcabd534492985987f06a022
Rozi <roziwebdev@gmail.com>
2024-09-19 20:49:09 +0700

    Provider Toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Clientside from "@/layouts/Clientside";
-import { Toaster } from "react-hot-toast";
+import ToastProvider from "@/provider/ToastProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,10 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Toaster />
+      <body>
+        <ToastProvider />
         <Clientside />
         {children}
       </body>
