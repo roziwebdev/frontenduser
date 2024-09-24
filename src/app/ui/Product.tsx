a16ef04c337c6d04ee82833edb1c6353ef7c6bc9
roziwebdev <145320304+roziwebdev@users.noreply.github.com>
2024-09-24 15:20:55 +0700

    Update Description Product

--- src/app/ui/Product.tsx
+++ src/app/ui/Product.tsx
@@ -37,13 +37,12 @@ export function Products() {
     <section className="px-8">
       <div className="container mx-auto mb-24 text-center">
         <Typography variant="h2" color="blue-gray"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          Explore Courses
+          Explore Products
         </Typography>
         <Typography
           variant="lead"
           className="mt-2 mx-auto w-full px-4 !text-gray-500 lg:w-6/12 lg:px-8"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}        >
-          Browse through 1,000+ web development courses and find the one that
-          fits your needs.
+          Explore our range of digital printing products We offer high-quality, vibrant prints that bring your packaging designs to life, perfect for branding and presentation.
         </Typography>
       </div>
       <div className="container mx-auto grid grid-cols-1 gap-x-10 gap-y-24 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-14">
